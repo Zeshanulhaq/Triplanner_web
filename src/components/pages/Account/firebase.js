6dee5f1bd586db3c0d01884436af2db89df3d66a
Zeshanulhaq <70916705+Zeshanulhaq@users.noreply.github.com>
2021-12-07 15:25:16 +0500

    new update

--- src/components/pages/Account/firebase.js
+++ src/components/pages/Account/firebase.js
@@ -1,14 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyA6bz_fZrzJgtIaS0Fbl0JiS1ENf1pg94A",
-  authDomain: "trip-planner-web-4997d.firebaseapp.com",
-  projectId: "trip-planner-web-4997d",
-  storageBucket: "trip-planner-web-4997d.appspot.com",
-  messagingSenderId: "1091636087519",
-  appId: "1:1091636087519:web:5a24d079e3c76dff50376a"
-});
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
 
-export const auth = app.auth();
-export default app;
+export const auth = app.auth()
+export default app
